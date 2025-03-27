682092fe35a29ef52bf22ca119a3e9c56131e606
JRE\charles.gougenhe.civ <charles.r.gougenheim.civ@us.navy.mil>
2025-03-27 09:59:13 -0700

    more code

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -11,5 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
+
+  connectDB();
 });
 // bACbCAgNICN4GDup
